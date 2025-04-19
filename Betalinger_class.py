e552b20f1fd7fbaac13b245836ea52e5e9648b6c
Peder Aa. Hoff <peder.aa.hoff@gmail.com>
2025-04-19 13:54:42 +0200

    Fiksa på lagNpArray

--- Betalinger_class.py
+++ Betalinger_class.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 måneder = ["Januar", "Februar", "Mars", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Desember"]
 
 class Betaling:
@@ -13,6 +15,15 @@ class Betaling:
         self.innPaaKonto = innPaaKonto
 
         self.kategorier = []
+    
+    def tonpArray(self):
+        row = np.empty(4, dtype=object)
+        row[0] = str(self.dato) + ". " + str(self.month)
+        row[1] = self.forklaring
+        row[2] = self.utFraKonto
+        row[3] = self.innPaaKonto
+
+        return row
 
 
 class Betalinger:
@@ -30,3 +41,26 @@ class Betalinger:
             utgift += betaling.utFraKonto
         
         return utgift, innskudd
+
+    def lagNpArray(self):
+        sheet = np.empty((len(self.list)+1, 4), dtype=object)
+        total = [0, 0]
+
+        for i in range(len(self.list)):
+            betaling = self.list[i]
+
+            r = betaling.tonpArray()
+            sheet[i][0] = r[0]
+            sheet[i][1] = r[1]
+            sheet[i][2] = r[2]
+            sheet[i][3] = r[3]
+
+            total[0] += betaling.utFraKonto
+            total[1] += betaling.innPaaKonto
+
+    
+        sheet[-1][1] = "Total:"
+        sheet[-1][2] = total[0]
+        sheet[-1][3] = total[1]
+    
+        return sheet
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from sorterBetalingerEtterKategori import sorterBetalingerEtterKategori
 
 filnavn = "okt23_nov24.xlsx"
 print("Hello world")
+
 # Ordner betalingene (egen class) i en egen liste fra exel-dokumentet
 betalinger = lagListeAvBetalinger(filnavn) 
 
@@ -13,3 +14,32 @@ betalingerSortertTid = sorterEtterAarmanneder(betalinger)
 
 # Sorterer betalingene i en dict hvor nøkkelen er etter hvilken kategori den tilhører
 betalingerSortertKatagori = sorterBetalingerEtterKategori(betalinger)
+
+"""
+print("Dato: ", end="")
+print(betalinger.list[0].datestamp)
+print("Forklaring: ", end="")
+print(betalinger.list[0].forklaring)
+print("Ut: ", end="")
+print(betalinger.list[0].utFraKonto)
+print("Inn: ", end="")
+print(betalinger.list[0].innPaaKonto)
+
+"""
+g = betalinger.lagNpArray()
+
+print("Dato: ", end="")
+print(g[0][0])
+print("Forklaring: ", end="")
+print(g[0][1])
+print("Ut: ", end="")
+print(g[0][2])
+print("Inn: ", end="")
+print(g[0][3])
+
+"""
+for i in range(len(g)):
+    print(str(g[0]) + " " + str(g[1]) + " " + str(g[2]) + " " + str(g[3]))
+    print()
+    print()
+"""

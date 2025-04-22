0a8d16c3003b3bd7141d72e0557ca855b85baf12
Peder Aa. Hoff <peder.aa.hoff@gmail.com>
2025-04-22 18:40:17 +0200

    Skrivefeil

--- slaaSammenDager.py
+++ slaaSammenDager.py
@@ -3,7 +3,7 @@ from Betalinger_class import Betalinger
 def slaaSammenDager(betalinger):
     ny_betalingsliste = Betalinger()
     ny_betalingsliste.append(betalinger[0])
-    ny_betalingsliste[-1].forkalring = None
+    ny_betalingsliste[-1].forklaring = None
 
     for betaling in betalinger[1:len(betalinger)]:
         if betaling.dato == ny_betalingsliste[-1].dato:
@@ -11,6 +11,6 @@ def slaaSammenDager(betalinger):
             ny_betalingsliste[-1].innPaaKonto += betaling.innPaaKonto
         else:
             ny_betalingsliste.append(betaling)
-            ny_betalingsliste[-1].forkalring = None
+            ny_betalingsliste[-1].forklaring = None
     
     return ny_betalingsliste

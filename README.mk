bf83a5b268df4e75ea18399a05fdc4518ce333dc
Peder Aa. Hoff <54407742+Pederaa@users.noreply.github.com>
2025-11-01 20:57:33 +0100

    Update README.mk

--- README.mk
+++ README.mk
@@ -1,10 +1,10 @@
-# Automatisk Økonomianalyse
+Automatisk Økonomianalyse
 Hensikten med dette prosjektet er å lage et program som kan hente inn og analysere kontoutskriften min og fremstille betalinger jeg har gjort på en oversiktlig måte. Målet er å bruke denne koden til å bli mer bevist på egen pengebruk og ta bedre økonomiske valg. 
 
 
 ## Klasser
 ### Betaling(self, datestamp, forklaring, utFraKonto, innPaaKonto)
-Lagrer en betaling som en instanse av en "betalingsklasse". Denne inneholder all informasjon om klassen, f.eks.:beløp inn og ut, dato, beskrivelse osv. Funksjoner:
+🤞Lagrer en betaling som en instanse av en "betalingsklasse". Denne inneholder all informasjon om klassen, f.eks.:beløp inn og ut, dato, beskrivelse osv. Funksjoner:
 - __eq__(self, other): Brukes for å se om to betalinger er like
 - __hash__(self): Lager en hash-id av betalingen (elns)
 - tonpArray(self, columns): Lager en (1, len(columns))-dimensional np-array av betalingen. 
